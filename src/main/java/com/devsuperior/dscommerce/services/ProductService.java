fa03fb468e518b77c945a8bcd67426d7ebe41724
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-08-29 19:05:36 -0300

    finalizacao da configuracao do controller

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,9 +1,8 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
@@ -15,9 +14,10 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> result = repository.findById(id);
-        Product product = result.get();
+
+        Product product = repository.findById(id).get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }

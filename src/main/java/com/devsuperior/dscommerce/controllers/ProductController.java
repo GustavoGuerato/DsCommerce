7495750ef3f701fcd06b7c9b01e5d74616cf18be
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-09-04 13:39:08 -0300

    criacao do custom error e algumas alteracoes no productcontroller

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.controllers;
 
 import java.net.URI;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devsuperior.dscommerce.dto.CustomError;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+import com.devsuperior.dscommerce.services.Exceptions.ResourceNotFoundException;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -27,9 +30,14 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
-        ProductDTO dto = service.findById(id);
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            ProductDTO dto = service.findById(id);
+            return ResponseEntity.ok(dto);
+        } catch (ResourceNotFoundException e) {
+            CustomError err = new CustomError(Instant.now(), 404, e.getMessage(), "caminho");
+            return ResponseEntity.status(404).body(err);
+        }
     }
 
     @GetMapping

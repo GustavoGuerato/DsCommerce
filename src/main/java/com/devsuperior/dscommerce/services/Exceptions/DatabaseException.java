3ca7accd0767b21702f87d24c4d3e9db7b354c84
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-09-04 18:10:24 -0300

    criacao do databaseexception

---
+++ src/main/java/com/devsuperior/dscommerce/services/Exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscommerce.services.Exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}

b677ad55ae1ae36b5c7e827f6b65a73631fea668
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-09-04 18:47:19 -0300

    utilizacao do bean validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,15 +2,22 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "Campo deve ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo Obrigatorio")
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter 10 caracteres no minimo")
+    @NotBlank(message = "Campo Obrigatorio")
     private String description;
+    @Positive(message = "o preço deve ser positivo")
     private Double price;
     private String imgUrl;
-
- 
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
